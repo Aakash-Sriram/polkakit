86dc78c7f16e97e0362ee3fec871b9bdb1316a73
Aakash Sriram <194975502+Aakash-Sriram@users.noreply.github.com>
2025-11-17 22:55:45 +0530

    get OrmTokens PalletAssets All assests together

--- src/lib/state.ts
+++ src/lib/state.ts
@@ -58,9 +58,81 @@ if (hasBalance) {
 export const getSupportedTokens = (api: ApiPromise) => {
   const symbols = api.registry.chainTokens || ["UNIT"];
   const decimals = api.registry.chainDecimals || [12];
-
   return symbols.map((symbol, i) => ({
     symbol,
     decimals: decimals[i] ?? decimals[0],
   }));
+};
+
+export const getAllOrmlTokens = async (api: ApiPromise,address:string) => {
+    if(!api.query.tokens || !api.query.tokens.accounts)return[];
+    try {
+        const entries = await api.query.tokens.metadata.entries();
+        const tokens = await Promise.all(
+            entries.map(async ([key, value]) => {
+                const [assetKey] = key.args;
+                const metaData = value.toJSON() as any;
+                const balance = await api.query.tokens.accounts(address, assetKey);
+                const jsonBal = balance.toJSON() as any;
+                return{
+                    id: assetKey.toString(),
+                    name: metaData.name || "Unknown",
+                    symbol: metaData.symbol || "UNK",
+                    decimals: metaData.decimals || 12,
+                    free: jsonBal?.free?.toString() ?? "0",
+                    reserved: jsonBal?.reserved?.toString() ?? "0",
+                    frozen: jsonBal?.frozen?.toString() ?? "0",
+                    source: "orml"  
+                };
+            })
+        );
+        return tokens;
+    }
+    catch(error:any){
+        console.error("Failed to fetch ORML tokens:", error?.message);
+        return [];
+    }
+}
+
+export const getAllPalletAssets = async (api: ApiPromise, address: string) => {
+  if (!api.query.assets || !api.query.assets.metadata) return [];
+
+  try {
+    const entries = await api.query.assets.metadata.entries();
+    const assets = await Promise.all(
+      entries.map(async ([key, meta]) => {
+        const [assetId] = key.args;
+        const metadata = meta.toJSON() as any;
+        const account = await api.query.assets.account(assetId, address);
+        const accJSON = account.toJSON() as any;
+        return {
+          id: assetId.toString(),
+          symbol: metadata.symbol || "ASSET",
+          name: metadata.name || "Asset",
+          decimals: metadata.decimals || 12,
+          free: accJSON?.balance?.toString() ?? "0",
+          frozen: accJSON?.frozen?.toString() ?? "0",
+          reserved: accJSON?.reserved?.toString() ?? "0",
+          source: "assets-pallet"
+        };
+      })
+    );
+
+    return assets;
+  } catch (err) {
+    console.error("Failed to fetch Assets pallet:", err);
+    return [];
+  }
+};
+
+export const getAllAssets = async (api: ApiPromise, address: string) => {
+  const native = await getBalance(api, address);
+  const orml = await getAllOrmlTokens(api, address);
+  const palletAssets = await getAllPalletAssets(api, address);
+
+  return {
+    native,
+    orml,
+    palletAssets
+  };
 };
--- src/test/state.ts
+++ src/test/state.ts
@@ -1,9 +1,12 @@
+import test from "node:test";
 import { createProvider } from "../lib/provider";
-import { getBalance, getSupportedTokens } from "../lib/state";
+import { getAllAssets, getAllOrmlTokens, getAllPalletAssets, getBalance, getSupportedTokens } from "../lib/state";
+import { ApiPromise } from "@polkadot/api";
+const ADDRESS = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY"
+const RPC = "wss://rpc.polkadot.io";
 const test1 = async () => {
-    const address = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
-    const api = await createProvider("wss://rpc.polkadot.io");
-    const acc = await api.query.system.account(address);
+    const api = await createProvider(RPC);
+    const acc = await api.query.system.account(ADDRESS);
     const JsonData = acc.toJSON();
     const {data} = JsonData as any;
     console.log("Account Data:", data);
@@ -11,18 +14,53 @@ const test1 = async () => {
 // test1().catch(console.error);
 
 const test2 = async () =>{
-    const api = await createProvider("wss://rpc.polkadot.io");
-    const address = "5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY";
-    const balance =  await getBalance(api, address);
+    const api = await createProvider(RPC);
+    const balance =  await getBalance(api, ADDRESS);
     console.log("Balance Info:", balance);
 }
 
 // test2().catch(console.error);
 
 const test3 = async () =>{
-    const api = await createProvider("wss://rpc.polkadot.io");
+    const api = await createProvider(RPC);
     const tokens = getSupportedTokens(api);
     console.log("Supported Tokens:", tokens);
 }
 
-test3().catch(console.error);
+// test3().catch(console.error);
+
+const testOrml = async (api:ApiPromise) => {
+
+    const tokens = await getAllOrmlTokens(api, ADDRESS);
+    console.log("\nORML Tokens");
+    console.log(tokens);
+    await api.disconnect();
+};
+
+const testPalletAssets = async (api:ApiPromise) => {
+
+    const assets = await getAllPalletAssets(api, ADDRESS);
+    console.log("\nPallet Assets");
+    console.log(assets);
+    await api.disconnect();
+};
+
+
+const testAll = async (api:ApiPromise) => {
+
+    const portfolio = await getAllAssets(api, ADDRESS);
+    console.log("\nFull Asset");
+    console.log(JSON.stringify(portfolio, null, 2));
+    await api.disconnect();
+
+};
+
+
+(async ()=>{
+    const api = await createProvider(RPC);
+    testOrml(api).catch(console.error);
+    testPalletAssets(api).catch(console.error);
+    testAll(api).catch(console.error);
+
+    }
+)();
